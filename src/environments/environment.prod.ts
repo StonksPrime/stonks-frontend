8b3e19f7be7a655cbc54b9a2040eb0019532676d
Marc Pascual <marcpascu@gmail.com>
2021-03-29 18:28:55 +0200

    get backend API host from environment variable, fix http interceptor filter

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,6 @@
-/**
- * @license
- * Copyright Akveo. All Rights Reserved.
- * Licensed under the MIT License. See License.txt in the project root for license information.
- */
 export const environment = {
   production: true,
-  apiUrl: 'http://192.168.0.120:4321/api',
+  apiUrl: $ENV.BACKEND_HOST,
   jwtLogin: 'token-auth/',
   jwtRefresh: 'token-refresh/',
 };
